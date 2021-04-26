5110cb755e1a0cca5bba78616a3ad8152c75fb7a
Kevin Hagen <hagen@threaks.com>
2021-04-26 11:25:00 +0200

    add animation events for footsteps

---
+++ Assets/Code/Day01/FootStepAudioHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+///		Class that forward an animation event to a <see cref="ScriptableAudioEvent"/>.
+/// </summary>
+public class FootStepAudioHandler : MonoBehaviour
+{
+	[SerializeField] private AudioSource _audioSource = default;
+	
+	public void DoFootStepSound(AnimationEvent animationEvent)
+	{
+		// simple play of sfx on an audio source:
+		// AudioClip clip = animationEvent.objectReferenceParameter as AudioClip;
+		// _audioSource.clip = clip;
+		// _audioSource.Play();
+		
+		// Usage of scriptable audio events:
+		ScriptableAudioEvent footStepAudioEvent = animationEvent.objectReferenceParameter as ScriptableAudioEvent;
+		if (footStepAudioEvent != null)
+		{
+			footStepAudioEvent.Play(_audioSource);
+		}
+	}
+}
