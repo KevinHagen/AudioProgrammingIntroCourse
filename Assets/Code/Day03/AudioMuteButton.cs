f99b3028b1be3e661b1e9e5d3d16af3fee798435
Kevin Hagen <hagen@threaks.com>
2021-04-27 11:18:15 +0200

    Add mute button

---
+++ Assets/Code/Day03/AudioMuteButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Day03
+{
+	public class AudioMuteButton : MonoBehaviour
+	{
+		[SerializeField] private Toggle _toggle;
+
+		private void Awake()
+		{
+			_toggle.onValueChanged.AddListener(OnMuteToggle);
+		}
+
+		private void OnDestroy()
+		{
+			_toggle.onValueChanged.RemoveListener(OnMuteToggle);
+		}
+
+		private void OnMuteToggle(bool active)
+		{
+			AudioListener.volume = active ? 0f : 1f;
+		}
+	}
+}
