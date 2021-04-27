5333458a9df0d463a02c5bc3fa4bdfceaa418112
Kevin Hagen <hagen@threaks.com>
2021-04-27 11:34:31 +0200

    Add save logic for audio settings

---
+++ Assets/Code/Day03/PlayerPrefsKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace Code.Day03
+{
+	public static class PlayerPrefsKeys
+	{
+		public static string SfxVolumeKey = "sfx_volume";
+		public static string MusicVolumeKey = "music_volume";
+		public static string VoiceOverVolumeKey = "vo_volume";
+		public static string MasterVolumeKey = "master_volume";
+	}
+}
---
+++ Assets/Code/Day03/SaveSettings.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Code.Day03
+{
+	public class SaveSettings : MonoBehaviour
+	{
+		[SerializeField] private AudioMixer _settingsMixer;
+		
+		private Button _button;
+
+		private void Awake()
+		{
+			_button = GetComponent<Button>();
+			_button.onClick.AddListener(SaveToSettings);
+		}
+
+		private void SaveToSettings()
+		{
+			_settingsMixer.GetFloat(AudioMixerParams.MasterVolumeKey, out float masterVolume);
+			_settingsMixer.GetFloat(AudioMixerParams.MusicVolumeKey, out float musicVolume);
+			_settingsMixer.GetFloat(AudioMixerParams.SfxVolumeKey, out float sfxVolume);
+			_settingsMixer.GetFloat(AudioMixerParams.VoiceOverVolumeKey, out float voiceOverVolume);
+			
+			PlayerPrefs.SetFloat(PlayerPrefsKeys.MasterVolumeKey, masterVolume);
+			PlayerPrefs.SetFloat(PlayerPrefsKeys.MusicVolumeKey, musicVolume);
+			PlayerPrefs.SetFloat(PlayerPrefsKeys.SfxVolumeKey, sfxVolume);
+			PlayerPrefs.SetFloat(PlayerPrefsKeys.VoiceOverVolumeKey, voiceOverVolume);
+			
+			PlayerPrefs.Save();
+		}
+	}
+}
