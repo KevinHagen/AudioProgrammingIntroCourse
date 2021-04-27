3dad65ad181fad4ac8566acc2965a93707223c2b
Kevin Hagen <hagen@threaks.com>
2021-04-27 09:40:52 +0200

    make settings adjust current value text for all audio sliders

---
+++ Assets/Code/Day03/AudioSlider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Day03
+{
+	public class AudioSlider : MonoBehaviour
+	{
+		[Header("Slider Setup")]
+		[Tooltip("The slider driving this audio slider")]
+		[SerializeField]
+		private Slider _slider = default;
+
+		[Tooltip("Text to output the sliders current value.")]
+		[SerializeField] 
+		private Text _currentValueText = default;
+
+		private void Awake()
+		{
+			_slider.onValueChanged.AddListener(OnSliderValueChanged);
+		}
+
+		private void OnDestroy()
+		{
+			_slider.onValueChanged.RemoveListener(OnSliderValueChanged);
+		}
+
+		private void OnSliderValueChanged(float value)
+		{
+			float displayValue = value * 100f;
+			_currentValueText.text = displayValue.ToString("000") + "%";
+		}
+	}
+}
