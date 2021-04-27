d7264a8243155474661335a103d21993c261417e
Kevin Hagen <hagen@threaks.com>
2021-04-27 14:11:06 +0200

    add timeline controller

---
+++ Assets/Code/Day03/CutsceneController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Code.Day03
+{
+	public class CutsceneController : MonoBehaviour
+	{
+		[SerializeField] private PlayableDirector _playableDirector;
+
+		private bool _isPaused;
+		
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				_playableDirector.Play();
+			}
+			
+			// if the timeline is playing: pause it - if not continue!
+			if (Input.GetKeyDown(KeyCode.L))
+			{
+				if (_isPaused)
+				{
+					_playableDirector.Resume();
+				}
+				else
+				{
+					_playableDirector.Pause();
+				}
+				_isPaused = !_isPaused;
+			}
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.CompareTag("Player"))
+			{
+				_playableDirector.Play();
+			}
+		}
+	}
+}
